a69e5a02dade61f520ce22c93a6e8a35c1436429
mehdimedkouri <mehdimedkouri4@gmail.com>
2024-11-23 20:23:07 +0100

    adding department microservice

---
+++ departement-microservice/src/main/java/ma/emsi/departementmicroservice/DepartementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.departementmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartementMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartementMicroserviceApplication.class, args);
+    }
+
+}
---
+++ departement-microservice/src/test/java/ma/emsi/departementmicroservice/DepartementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.departementmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartementMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
