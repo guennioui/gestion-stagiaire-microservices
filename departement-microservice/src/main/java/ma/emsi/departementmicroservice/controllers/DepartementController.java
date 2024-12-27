c36d57f45d44b601dde771e866a2b003eccff27f
guennioui <guenniouiabdellah2002@gmail.com>
2024-12-27 15:26:05 +0100

    fixing bug at departement service

--- departement-microservice/src/main/java/ma/emsi/departementmicroservice/controllers/DepartementController.java
+++ departement-microservice/src/main/java/ma/emsi/departementmicroservice/controllers/DepartementController.java
@@ -5,6 +5,7 @@ import ma.emsi.departementmicroservice.entities.Departement;
 import ma.emsi.departementmicroservice.exceptions.DepartementAlreadyExistException;
 import ma.emsi.departementmicroservice.exceptions.DepartementNotFoundException;
 import ma.emsi.departementmicroservice.services.IService.IDepartementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,31 +14,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/departement")
+@CrossOrigin("*")
 public class DepartementController {
     private final IDepartementService iDepartementService;
 
+    @Autowired
     public DepartementController(IDepartementService iDepartementService) {
         this.iDepartementService = iDepartementService;
     }
-
 
     @PostMapping(path = "/add-departement")
     public ResponseEntity<?> addDepartement (@RequestBody DepartementDto departementDto) throws DepartementNotFoundException, DepartementAlreadyExistException {
         this.iDepartementService.addDepartement(departementDto);
         return ResponseEntity.ok(departementDto);
     }
+
     @PutMapping(path = "/update/{code}")
     public ResponseEntity<?> updateDepartement(@PathVariable String code, @RequestBody DepartementDto departementDto)
             throws DepartementNotFoundException {
         this.iDepartementService.updateDepartement(code, departementDto);
         return ResponseEntity.ok(departementDto);
     }
+
     @DeleteMapping(path = "/delete/{code}")
     public ResponseEntity<Void> deleteDepartement(@PathVariable String code)
             throws DepartementNotFoundException {
         this.iDepartementService.deleteDepartement(code);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
     @GetMapping(path = "/{code}")
     public ResponseEntity<DepartementDto> findDepartementByCode(@PathVariable String code)
             throws DepartementNotFoundException{
@@ -45,6 +50,7 @@ public class DepartementController {
         DepartementDto result = this.iDepartementService.DepartementToDepartementDto(departement);
         return ResponseEntity.ok(result);
     }
+
     @GetMapping(path = "/all")
     public ResponseEntity<List<DepartementDto>> getAll(){
         List<DepartementDto> all = this.iDepartementService .getAll();
--- departement-microservice/src/main/java/ma/emsi/departementmicroservice/services/IServicesImp/IDepartementServiceImpl.java
+++ departement-microservice/src/main/java/ma/emsi/departementmicroservice/services/IServicesImp/IDepartementServiceImpl.java
@@ -9,12 +9,14 @@ import ma.emsi.departementmicroservice.repositories.DepartementRepository;
 import ma.emsi.departementmicroservice.services.IService.IDepartementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class IDepartementServiceImpl implements IDepartementService {
     private final DepartementRepository departementRepository;
 
