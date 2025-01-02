6bad7385b933978187733a1fa640865dc21ad18b
mehdimedkouri <mehdimedkouri4@gmail.com>
2025-01-03 00:44:09 +0100

    merging encadrant model

--- front-end/src/app/models/encadrant.model.ts
+++ front-end/src/app/models/encadrant.model.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export interface Encadrant{
    matricule: string;
    nom: string;
@@ -7,14 +7,5 @@ export interface Encadrant{
    telephone: number;
    specialite: string;
 }
-||||||| 4eaaf3c
-=======
-export interface Encadrant{
-  matricule: string;
-  nom: string;
-  prenom: string;
-  email: string;
-  telephone: string;
-  specialite: string;
-}
->>>>>>> 58d493139b3f96e740d81fefec772c938684ff08
+
+
